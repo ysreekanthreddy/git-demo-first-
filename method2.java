c0fc994fd6d1a1c5776b9d290ce33d69cf47bc59
ysreekanthreddy <sreekanthsreekanth4by@gmail.com>
2025-04-28 22:20:29 +0530

    5th prgrm

---
+++ method2.java
@@ -0,0 +1,13 @@
+package normalpractice;
+
+public class method2 {
+
+	public static void main(String[] args) { 
+      	System.out.println(	topend());
+	}
+	
+ public static   int topend() {
+	 System.out.println(" tata safari ");
+	 return 99999;
+ }
+}
