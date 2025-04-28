13d6518816eb1d555f03668e929e37f7e78e1a87
ysreekanthreddy <sreekanthsreekanth4by@gmail.com>
2025-04-28 22:13:32 +0530

    3rd prgrm

---
+++ method.java
@@ -0,0 +1,18 @@
+package normalpractice;
+
+public class method {
+  int a =10;
+  int b =20;
+public static void main(String[] args) {
+method m1 =new method();
+int c = m1.add();
+System.out.println(c);
+}
+	public  int add() {
+		 
+	System.out.println(a+b);
+		return 999;
+		
+	}
+
+}
